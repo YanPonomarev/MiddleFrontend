3366f2a660ddc03fbfe34b33722a7a20de8592f6
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2021-08-28 00:05:05 +0300

    Сделала первый вариант dialog-window привязка к yard
    Создала первый вариант dialog-window,
    пока без трансформации в высоту.
    Привязала к yard для отображения трудов.

---
+++ src/components/dialog-window/dialog-window.js
@@ -0,0 +1,5 @@
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import template from './dialog-window.hbs';
+import './dialog-window.scss';
+
+Handlebars.registerPartial('dialog-window', template);
